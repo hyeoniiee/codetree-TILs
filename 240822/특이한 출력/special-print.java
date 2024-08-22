54ec3d419f5b0511513e53e3aafc43b5b12ccefc
heeeiiii <starjea0126@gmail.com>
2024-08-22 12:28:04 +0900

    Title: 특이한 출력, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240822/특이한 출력/special-print.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("(" + i + ", " + j + ")");
+                
+                if ((i + j) % 4 == 0) {
+                    System.out.println(); 
+                } else if (j != n) {
+                    System.out.print(" ");
+                }
+            }
+
+            if ((i + n) % 4 != 0) {
+                System.out.print(" ");
+            }
+        }
+    }
+}
