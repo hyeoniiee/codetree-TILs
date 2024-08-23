a4d821aac4a8bfabfad8330c16370286a1ccf55a
heeeiiii <starjea0126@gmail.com>
2024-08-23 09:41:08 +0900

    Title: 3n + 1 수열 m번, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240823/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int m = sc.nextInt();
+        
+        for (int i = 0; i < m; i++) {
+            int n = sc.nextInt();
+            int count = 0;
+            
+            while (n != 1) {
+                if (n % 2 == 0) {
+                    n = n / 2;
+                } else {
+                    n = n * 3 + 1;
+                }
+                count++;
+            }
+            
+            System.out.println(count);
+        }
+    }
+}
