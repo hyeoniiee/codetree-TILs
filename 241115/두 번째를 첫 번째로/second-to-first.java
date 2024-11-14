6cdbc9b133feca3b14efa86a6a37f90025568b19
heeeiiii <starjea0126@gmail.com>
2024-11-15 00:11:35 +0900

    Title: 두 번째를 첫 번째로, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241115/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+
+        char firstChar = input.charAt(0);
+        char secondChar = input.charAt(1);
+
+        StringBuilder result = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if ( c == secondChar) {
+                result.append(firstChar);
+            } else {
+                result.append(c);
+            }
+        }
+
+        System.out.println(result.toString());
+    }
+}
